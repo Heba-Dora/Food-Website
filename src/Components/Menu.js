5bb91fc5d6478bd1a7c16e7ec2f0a5eb92bf9bc7
Heba <hebamirdora@gmail.com>
2021-08-28 23:26:00 +0200

    Menu page added

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import dishes from './dishes'
+import '../Styles/Menu.css'
 
 function Menu() {
     return (
-        <div>
-            <h1>Menu List</h1>
-            
+        <div className="menuList">
+            {dishes.map((dish, id)=>{
+                return(
+                    <div className="dishesList" key={id} >
+                        <h4>Price: {dish.price} €</h4>
+                        <div className="foodImage" style={{ display: 'flex', flexWrap:'wrap',  position:'relative', width:'50%', gridTemplateRows: 'auto 1fr'}}>{dish.image}</div>
+                        <h2 >{dish.name}</h2>
+                        <h5>{dish.description}</h5>
+                        
+
+                    </div>
+                )
+            })
+
+            }            
         </div>
     )
 }
---
+++ src/Components/dishes.js
@@ -0,0 +1,72 @@
+const DISHES =
+    [
+        {
+        id: 0,
+        name:'BBQ Chicken',
+        image: <img   style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}}  src='https://images.immediate.co.uk/production/volatile/sites/2/2015/04/5047.jpg?webp=true&quality=45&resize=1240%2C826' alt=""/>,
+        price:'4.99',
+        description:'This version comes with our secret sauce (made from four very simple ingredients), sweet and sour onions, emmental cheese, gherkins, frilly lettuce, and more.',
+        },
+        {
+        id: 1,
+        name:'Chicken Wings',
+        image: <img alt=""  style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}}  src='https://images.immediate.co.uk/production/volatile/sites/2/2019/07/Chic-wings-b9d5fd6.jpg?webp=true&quality=45&resize=1240%2C826'/>,
+        price:'1.99',
+        description:'These juicy wings are spiced with paprika, cumin, cayenne pepper and fresh oregano. Crisp up on the BBQ and serve with a light yogurt sauce to feed a hungry horde.',
+        
+        },
+        {
+        id: 2,
+        name:'BBQ Kebab',
+        image: <img alt=""  style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}}  src='https://images.immediate.co.uk/production/volatile/sites/2/2019/07/Adana-Kebab-f49ae24.jpg?webp=true&quality=45&resize=1240%2C826'/>,   
+        price:'1.99',
+        description:'Make these Turkish-style lamb kebabs at your next BBQ. They’re named after the city of Adana and flavoured with chilli and sumac.', 
+        },
+        {
+        id: 3,
+        name:'BBQ Sausages',
+        image: <img alt=""  style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}}  src='https://images.immediate.co.uk/production/volatile/sites/2/2020/07/OLI-0720_GetItRight-BBQ-CarolinaMustardBBQ_04500_preview-70200c0.jpg?webp=true&quality=90&resize=620%2C413'/>,
+        price:'2.99',
+        description:'Fiery, piquant and less sweet than other varieties, Carolina BBQ sauce complements caramelised, smoky meat perfectly. Get it involved with your next BBQ using this great recipe that serves eight.',
+        },
+        {
+        id: 4,
+        name:'BBQ Brisket',
+        image: <img alt=""  style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}}  src="https://images.immediate.co.uk/production/volatile/sites/2/2017/06/Brisket_Final.jpg?webp=true&quality=45&crop=7px%2C905px%2C4473px%2C2980px&resize=1240%2C826"/>,
+        price:'2.32',
+        description: 'Get the most out of your BBQ this summer with our seriously tender, home-smoked brisket. We’ve included some tricks and tips to help you nail this recipe, every time.',
+        },
+        {
+        id: 5,
+        name:'Hot-Smoked Salmon',
+        image: <img alt=""  style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}}  src="https://images.immediate.co.uk/production/volatile/sites/2/2021/08/OLI_0921_FREEZER-ENTERTAINING-3-10-THINGS-INSPIRATION-ROSIE-PHOTOGRAPHY-HAYLEY-BENOIT-LONDON-JULY-2021-TIFF-FULL-SIZE-7--fd675f5.jpg?webp=true&quality=45&resize=1880%2C799"/>,
+        price:'1.48',
+        description: 'Make at-home entertaining even easier with these hot-smoked salmon vol-au-vents that can be rustled up ahead of time. Simply re-heat and assemble to serve!',
+        },
+        {
+        id: 6,
+        name: 'Brazil Burger',
+        image: <img alt=""  style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}} 
+        src="https://images.immediate.co.uk/production/volatile/sites/2/2016/07/24531.jpg?webp=true&quality=45&resize=1201%2C800"/>,
+        price:'3.45',
+        description: 'All the flavours of Brazil are crammed into one epic mouthful here – peri peri bacon, smoky chimichurri relish, cumin-spiced onion rings, creamy lime and avocado mayo – it’s a bit of extra effort but take the challenge and you’ll be rewarded with a gold-medal BBQ burger.',
+        },
+        {
+        id: 7,
+        name:'BBQ Lamb',
+        image: <img alt="" style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}} 
+          src="https://images.immediate.co.uk/production/volatile/sites/2/2019/07/Breast-of-lamb-70a8f00.jpg?webp=true&quality=45&resize=1240%2C826"/>,
+        price:'2.25',
+        description: 'This malt vinegar sauce is a British riff on a Carolina-style BBQ sauce and makes a great acidic partner for rich lamb, cooked over coals.',
+        },
+        {
+        id: 8,
+        name:'Vegan Mushroom',
+        image: <img alt=""  style={{objectPosition:'center', objectFit:'cover', display: 'flex', borderRadius:'50%', width:'300px', height:'200px', flexWrap: 'wrap',gridTemplateRows: 'auto 1fr'}} src="https://images.immediate.co.uk/production/volatile/sites/2/2018/01/OLI0118-BumperHealthy-MushroomBolognese_020264-77f4f4d.jpg?webp=true&quality=45&resize=1880%2C799"/>,
+        price:'3.99',
+        description: 'Mushroom bolognese meat-free with a rich ragu of dried porcini and chestnut mushrooms, and plenty of vegetables for a low-fat, vegan meal',
+        },
+    ];
+
+
+    export default DISHES;
